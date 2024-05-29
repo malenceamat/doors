301422cf204f5c6c12ecf5fd0546a27bccbe062d
malenceamat <yatikivan@gmail.com>
2024-05-29 11:44:06 +0300

    правки

--- resources/views/user/content/about.blade.php
+++ resources/views/user/content/about.blade.php
@@ -5,7 +5,9 @@
 
             <div class="col-sm-6 col-lg-5 col-sm-push-6 col-lg-push-7 index-about-img-wrapper">
                 <div class="index-about-img">
-                    <img src="{{ $blog->image ? asset('/storage/' . $blog->image) : asset('') }}" alt="image">
+                    @if ($blog->image)
+                        <img src="{{ asset('/storage/' . $blog->image) }}" alt="image">
+                    @endif
                 </div>
                 <div class="push50 visible-xs"></div>
             </div>
