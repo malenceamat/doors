c6afd260bb8988369b16753d793366c574505fb1
malenceamat <yatikivan@gmail.com>
2024-12-02 17:52:38 +0300

    123

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Http;
 
 use App\Http\Middleware\EnsureUserHasRole;
+use App\Http\Middleware\Verified_1C;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 
 class Kernel extends HttpKernel
