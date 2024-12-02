190f2dd223cd348d50134de88df19e9c2c4cb28a
malenceamat <yatikivan@gmail.com>
2024-12-02 18:02:15 +0300

    123

--- app/Models/Items.php
+++ app/Models/Items.php
@@ -4,11 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
+
 class Items extends Model
 {
     use HasFactory;
 
     protected $table = 'items';
+
+    protected $fillable = ['name','category_id','id_1c','description','is_active','is_new_items','is_stock','is_popular','npp'];
 
     public function category()
     {
@@ -17,5 +21,18 @@ class Items extends Model
     public function entity()
     {
         return $this->hasMany(EntityItem::class);
+    }
+
+    public function getIsFavoritedAttribute()
+    {
+        if(Auth::check()) {
+            return $this->favorites()->where('user_id', Auth::id())->exists();
+        }
+        return false;
+    }
+
+    public function favorites()
+    {
+        return $this->hasMany(Favorites::class,'item_id','id');
     }
 }
